c91c1634f45628f47c89f07cecfff405df0538b2
Maximilian Konzack <m.konzack@gmx.de>
2013-07-12 16:15:34 +0200

    Initial add of dumb main.rb loadin fxml file

---
+++ lib/main.rb
@@ -0,0 +1,20 @@
+require 'jrubyfx'
+
+fxml_root File.join(File.dirname(__FILE__), "ui")
+
+class JRubyVisualizerApp < JRubyFX::Application
+  def start(stage)
+    with(stage, title: "JRuby Visualizer") do
+      fxml JRubyVisualizerController
+      show
+    end
+  end
+end
+
+class JRubyVisualizerController
+  include JRubyFX::Controller
+  fxml "jruby-visualizer.fxml"
+end
+
+JRubyVisualizerApp.launch
+
