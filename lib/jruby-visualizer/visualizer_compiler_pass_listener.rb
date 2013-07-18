58dd760381ca9365e316821972ba3d21832d0733
Maximilian Konzack <m.konzack@gmx.de>
2013-07-18 16:13:20 +0200

    Added puts for listener, in order to see if the listener was correctly injected to IRManager

--- lib/jruby-visualizer/visualizer_compiler_pass_listener.rb
+++ lib/jruby-visualizer/visualizer_compiler_pass_listener.rb
@@ -7,16 +7,19 @@ class VisualizerCompilerPassListener
   
   java_signature 'public void alreadyExecuted(CompilerPass, IRScope, Object, boolean)'
   def already_executed(pass, scope, data, child_scope)
+    puts "PassListener inside already_executed"
     nil
   end
   
   java_signature 'public void startExecute(CompilerPass, IRScope, boolean)'
   def start_execute(pass, scope, child_scope)
+    puts "PassListener inside start_execute"
     nil
   end
   
   java_signature 'public void endExecute(CompilerPass, IRScope, Object, boolean)'
   def end_execute(pass, scope, data, child_scope)
+    puts "PassListener inside end_execute"
     nil
   end
 end
