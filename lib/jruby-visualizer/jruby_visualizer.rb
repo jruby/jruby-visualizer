9f46efabda5bd4fbbf5c991e3124b01a37e70de1
Maximilian Konzack <m.konzack@gmx.de>
2013-07-18 16:36:47 +0200

    Simple visualize module method for a dry run on ruby source; Without starting the GUI, yet. Just to test the listener

--- lib/jruby-visualizer/jruby_visualizer.rb
+++ lib/jruby-visualizer/jruby_visualizer.rb
@@ -15,4 +15,30 @@ module JRubyVisualizer
   def self.has_pass_listener
     JRuby::IR.visualize
   end
+  
+  def self.visualize(ruby_code)
+    return unless has_pass_listener
+    # TODO start GUI
+    
+    # parse ruby_code into AST
+    root_node = JRuby.parse(ruby_code)
+    
+    builder = set_up_ir_builder
+    scope = builder.build_root(root_node)
+    
+    # TODO visualize AST and scope
+  end
+  
+  def self.set_up_ir_builder
+    ir_manager = JRuby::runtime.ir_manager
+    ir_manager.dry_run = true
+
+    builder = 
+      if JRuby::runtime.is1_9?
+      org.jruby.ir.IRBuilder19
+    else
+      org.jruby.ir.IRBuilder
+    end
+    builder = builder.new(ir_manager)
+  end
 end
