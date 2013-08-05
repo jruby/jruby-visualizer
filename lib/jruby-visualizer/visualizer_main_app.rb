32e18ec0b11a0bd117d855699a740f88a4fa50ee
Maximilian Konzack <m.konzack@gmx.de>
2013-08-05 10:21:44 +0200

    fill ast view with content

--- lib/jruby-visualizer/visualizer_main_app.rb
+++ lib/jruby-visualizer/visualizer_main_app.rb
@@ -1,8 +1,16 @@
 require 'jrubyfx'
+require_relative 'ast_tree_view_builder'
 
 fxml_root File.join(File.dirname(__FILE__), "ui")
 
 class VisualizerMainApp < JRubyFX::Application
+  
+  def fill_ast_view(stage, root_node)
+    tree_view = stage['#ast_view']
+    tree_builder = ASTTreeViewBuilder.new(tree_view)
+    tree_builder.build_view(root_node)
+  end
+  
   def start(stage)
     with(stage, title: "JRuby Visualizer") do
       fxml JRubyVisualizerController
