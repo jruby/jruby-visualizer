8589dba55bfd3b9e14cb9f46803a6ba5f8b245f4
Maximilian Konzack <m.konzack@gmx.de>
2013-08-06 17:13:24 +0200

    Binding of ir_scope to redrawing of #ir_view

--- lib/jruby-visualizer/ir_visualizer.rb
+++ lib/jruby-visualizer/ir_visualizer.rb
@@ -1,11 +1,18 @@
 require 'jrubyfx'
+require_relative 'ir_pretty_printer'
 
 fxml_root File.join(File.dirname(__FILE__), "ui")
 
 class IRVisualizer < JRubyFX::Application
+  
+  def init
+    # TODO read compiler data from main app
+    @compiler_data = nil
+  end
+  
   def start(stage)
     with(stage, title: "Intermediate Representation (IR) Visualizer") do
-      fxml IRVisualizerController
+      fxml(IRVisualizerController, initialize: [@compiler_data])
       show
     end
   end
@@ -14,4 +21,15 @@ end
 class IRVisualizerController
   include JRubyFX::Controller
   fxml "ir-view.fxml"
+  
+  attr_accessor :compiler_data
+  
+  def initialize(compiler_data)
+    @compiler_data = compiler_data
+    @compiler_data.ir_scope_property.add_change_listener do |new_scope|
+      pretty_ir_string = IRPrettyPrinter.print_ir(new_scope)
+      puts pretty_ir_string
+      @ir_view.text = pretty_ir_string
+    end
+  end
 end
