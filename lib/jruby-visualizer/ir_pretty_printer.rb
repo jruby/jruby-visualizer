d8b084319dc39a1902af666b734fbfedbd02275a
Maximilian Konzack <m.konzack@gmx.de>
2013-08-06 17:02:07 +0200

    pretty printer for IR from ast/bin

---
+++ lib/jruby-visualizer/ir_pretty_printer.rb
@@ -0,0 +1,24 @@
+module IRPrettyPrinter
+  
+  def self.pretty_ir(scope, indent="")
+    i = 0
+    pretty_str = scope.instrs.map do |instr|
+      f_str = "%s%3i\s\s%s" % [indent, i, instr]
+      i += 1
+      f_str 
+    end
+    pretty_str = [indent + scope.to_s] + pretty_str
+    scope.lexical_scopes.each do |lex_scope|
+      pretty_str += pretty_ir(lex_scope, indent + "\s" * 4)
+    end
+    pretty_str
+  end
+
+  def self.print_ir(scope)
+    instrs = pretty_ir(scope)
+    instrs.each do |instr|
+      puts instr
+    end
+  end
+  
+end
