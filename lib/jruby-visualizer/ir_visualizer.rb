a63f4cc8d69b8124ce5ae1c73820ba80e94fbbdb
Maximilian Konzack <m.konzack@gmx.de>
2013-08-07 14:54:57 +0200

    Removal of init() and add of simple launch, no app logic for ir_view

--- lib/jruby-visualizer/ir_visualizer.rb
+++ lib/jruby-visualizer/ir_visualizer.rb
@@ -5,14 +5,10 @@ fxml_root File.join(File.dirname(__FILE__), "ui")
 
 class IRVisualizer < JRubyFX::Application
   
-  def init
-    # TODO read compiler data from main app
-    @compiler_data = nil
-  end
-  
   def start(stage)
+    compiler_data = nil
     with(stage, title: "Intermediate Representation (IR) Visualizer") do
-      fxml(IRVisualizerController, initialize: [@compiler_data])
+      fxml(IRVisualizerController, initialize: [compiler_data])
       show
     end
   end
@@ -25,11 +21,16 @@ class IRVisualizerController
   attr_accessor :compiler_data
   
   def initialize(compiler_data)
-    @compiler_data = compiler_data
-    @compiler_data.ir_scope_property.add_change_listener do |new_scope|
-      pretty_ir_string = IRPrettyPrinter.print_ir(new_scope)
-      puts pretty_ir_string
-      @ir_view.text = pretty_ir_string
-    end
+    puts "inside of ir_visualizer controller"
+#    @compiler_data = compiler_data
+#    @compiler_data.ir_scope_property.add_change_listener do |new_scope|
+#      pretty_ir_string = IRPrettyPrinter.print_ir(new_scope)
+#      puts pretty_ir_string
+#      @ir_view.text = pretty_ir_string
+#    end
   end
+end
+
+if __FILE__ == $0
+  IRVisualizer.launch
 end
