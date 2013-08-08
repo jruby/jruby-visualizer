e15e9ab4b0338f6e76ae15c3e799d6ad9b5472b2
Maximilian Konzack <m.konzack@gmx.de>
2013-08-08 15:38:39 +0200

    Open IR window separately and integration of pretty printer to changes of ir_scope property

--- lib/jruby-visualizer/ir_visualizer.rb
+++ lib/jruby-visualizer/ir_visualizer.rb
@@ -1,12 +1,13 @@
 require 'jrubyfx'
 require_relative 'ir_pretty_printer'
+require_relative 'jruby_visualizer'
 
 fxml_root File.join(File.dirname(__FILE__), "ui")
 
 class IRVisualizer < JRubyFX::Application
   
   def start(stage)
-    compiler_data = nil
+    compiler_data = JRubyVisualizer.compiler_data
     with(stage, title: "Intermediate Representation (IR) Visualizer") do
       fxml(IRVisualizerController, initialize: [compiler_data])
       show
@@ -21,15 +22,18 @@ class IRVisualizerController
   attr_accessor :compiler_data
   
   def initialize(compiler_data)
-#    @compiler_data = compiler_data
-#    @compiler_data.ir_scope_property.add_change_listener do |new_scope|
-#      pretty_ir_string = IRPrettyPrinter.print_ir(new_scope)
-#      puts pretty_ir_string
-#      @ir_view.text = pretty_ir_string
-#    end
+    @compiler_data = compiler_data
+    pretty_ir_string = IRPrettyPrinter.pretty_string(@compiler_data.ir_scope)
+    @ir_view.text = pretty_ir_string
+    @compiler_data.ir_scope_property.add_change_listener do |new_scope|
+      pretty_ir_string = IRPrettyPrinter.pretty_string(new_scope)
+      @ir_view.text = pretty_ir_string
+    end
   end
+  
 end
 
 if __FILE__ == $0
+  JRubyVisualizer.compiler_data = CompilerData.new("i = 3 + 1; puts i")
   IRVisualizer.launch
 end
