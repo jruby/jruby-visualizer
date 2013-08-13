9aa57aee71e02e8c03eb7a94ce2e3abb616fb87d
Maximilian Konzack <m.konzack@gmx.de>
2013-08-13 14:59:03 +0200

    Controling ruby input (ruby string or ruby file) for the visualizer

--- lib/jruby-visualizer.rb
+++ lib/jruby-visualizer.rb
@@ -1,3 +1,17 @@
 require 'java'
 
 require_relative 'jruby-visualizer/jruby_visualizer'
+
+if __FILE__ == $0
+  unless ARGV.length == 1
+    raise %Q#No ruby input for the JRuby Visualizer\nUsage as argument: "def foo; puts 42; end; foo" or foo.rb#
+  end
+  ruby_input = ARGV[0]
+  ruby_code = if ruby_input.end_with?('.rb')
+    # TODO enable visualize with 'foo.rb' files
+    File.read(ruby_input)
+  else
+    ruby_input
+  end
+  JRubyVisualizer.visualize(ruby_code)
+end
