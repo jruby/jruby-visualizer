066f1214fa45d852432cbd150a0ebb2f2a63087d
Maximilian Konzack <m.konzack@gmx.de>
2013-08-15 16:47:51 +0200

    Add of new ir_scopes attribute to maintain the ir_scopes in hash

--- lib/jruby-visualizer/cfg_visualizer.rb
+++ lib/jruby-visualizer/cfg_visualizer.rb
@@ -1,5 +1,6 @@
 require 'jrubyfx'
 require_relative 'jruby_visualizer'
+require_relative 'ir_scope_registry'
 
 fxml_root File.join(File.dirname(__FILE__), "ui")
 
@@ -17,11 +18,22 @@ class CFGVisualizerController
   include JRubyFX::Controller
   fxml "cfg-view.fxml"
   
-  attr_reader :compiler_data
+  attr_reader :compiler_data, :ir_scopes
   
   def initialize(compiler_data)
     @compiler_data = compiler_data
+    
+    # read scopes into the registry
+    @ir_scopes = IRScopeRegistry.new(@compiler_data.ir_scope)
+    # listen to changes of the ir_scope property
+    @compiler_data.ir_scope_property.add_invalidation_listener do |new_scope_property|
+      root_scope = new_scope_property.get
+      @ir_scopes.clear
+      @ir_scopes.fill_registry(root_scope)
+      # TODO read the new scopes into the UI
+    end
   end
+  
 end
 
 if __FILE__ == $0
