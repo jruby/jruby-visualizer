576b60d5fa3fbc99a598c56f329f67baf3c58e5e
Maximilian Konzack <m.konzack@gmx.de>
2013-08-16 14:21:51 +0200

    Key in ir scopes Registry is a symbol similiar to IRScope#to_string

--- lib/jruby-visualizer/ir_scope_registry.rb
+++ lib/jruby-visualizer/ir_scope_registry.rb
@@ -1,6 +1,11 @@
 
 class IRScopeRegistry
+  
   attr_reader :scopes
+  
+  def self.ir_scope_to_key(ir_scope)
+    "#{ir_scope.get_name}[#{ir_scope.get_file_name}:#{ir_scope.get_line_number}]".to_sym
+  end
   
   def initialize(root_scope)
     @scopes = {}
@@ -8,7 +13,7 @@ class IRScopeRegistry
   end
   
   def fill_registry(ir_scope)
-    @scopes[ir_scope.to_s] = ir_scope
+    @scopes[self.class.ir_scope_to_key(ir_scope)] = ir_scope
     ir_scope.lexical_scopes.each do |lex_scope|
       fill_registry(lex_scope)
     end
