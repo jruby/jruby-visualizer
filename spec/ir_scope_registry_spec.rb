4a34437b5ce46d93f6c463db085972df8525ea95
Maximilian Konzack <m.konzack@gmx.de>
2013-08-16 14:36:31 +0200

    Add of spec for correct formatting of keys and update of example such that each scope is in a different line of the string

--- spec/ir_scope_registry_spec.rb
+++ spec/ir_scope_registry_spec.rb
@@ -5,7 +5,7 @@ describe IRScopeRegistry do
   include JRubyVisualizerTestUtils
   
   before(:each) do
-    @ruby_code = "class Foo; def foo; 42; end; end; Foo.new.foo"
+    @ruby_code = "\nclass Foo\ndef foo; 42; end; end;\nFoo.new.foo"
     ast_root = ast_for(@ruby_code)
     @root_scope = ir_scope_for(ast_root)
     @ir_reg = IRScopeRegistry.new(@root_scope)
@@ -19,5 +19,12 @@ describe IRScopeRegistry do
   it "should contain three scopes for #{@ruby_code}" do
     @ir_reg.scopes.size.should be(3)
   end
+  
+  it "should format the file scope - to a symbol" do
+    file_scope_symbol = IRScopeRegistry.ir_scope_to_key(@root_scope)
+    file_scope_symbol.should be_an_instance_of Symbol
+    file_scope_symbol.should == :"-[-:0]"
+  end
+  
 end
 
