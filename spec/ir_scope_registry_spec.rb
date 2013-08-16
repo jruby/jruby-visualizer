eae6e5b699865471aca3a2926dfe6969700b9ae3
Maximilian Konzack <m.konzack@gmx.de>
2013-08-16 14:42:46 +0200

    Further specs on the content of the registry

--- spec/ir_scope_registry_spec.rb
+++ spec/ir_scope_registry_spec.rb
@@ -5,7 +5,7 @@ describe IRScopeRegistry do
   include JRubyVisualizerTestUtils
   
   before(:each) do
-    @ruby_code = "\nclass Foo\ndef foo; 42; end; end;\nFoo.new.foo"
+    @ruby_code = "\nclass Foo\n\ndef bar; 42; end; end;\nFoo.new.bar"
     ast_root = ast_for(@ruby_code)
     @root_scope = ir_scope_for(ast_root)
     @ir_reg = IRScopeRegistry.new(@root_scope)
@@ -23,7 +23,19 @@ describe IRScopeRegistry do
   it "should format the file scope - to a symbol" do
     file_scope_symbol = IRScopeRegistry.ir_scope_to_key(@root_scope)
     file_scope_symbol.should be_an_instance_of Symbol
-    file_scope_symbol.should == :"-[-:0]"
+    file_scope_symbol.should == :'-[-:0]'
+  end
+  
+  it "should contain the file scope - at line 0" do
+   @ir_reg.scopes.should include(:'-[-:0]')
+  end
+  
+  it "should contain the Foo (class) scope at line 1" do
+    @ir_reg.scopes.should include(:'Foo[-:1]')
+  end
+  
+  it "should contain the scope for the method bar at line 3" do
+    @ir_reg.scopes.should include(:'bar[-:3]')
   end
   
 end
