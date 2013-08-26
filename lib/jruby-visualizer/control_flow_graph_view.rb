ad6e372af7aa8e71b532b4e18b94743cf7f89409
Maximilian Konzack <m.konzack@gmx.de>
2013-08-26 16:13:42 +0200

    Propposal of the creation for the basic blocks. Implementation for custom ListCell not there yet

--- lib/jruby-visualizer/control_flow_graph_view.rb
+++ lib/jruby-visualizer/control_flow_graph_view.rb
@@ -1,13 +1,25 @@
 require 'jrubyfx'
 
-class BasicBlockView < Java::javafx.scence.control.ListCell
-  # TODO compose with a textarea and buttons for successors of this basic block
+class BasicBlockListCell < Java::javafx.scence.control.ListCell
+  def initialize
+    # TODO compose with a textarea and buttons for successors of this basic block
+  end
+  
+  def updateItem
+    # TODO update layout with a new layout manager
+    # |bb_instrs|links to successors|
+  end
+  
 end
 
 class ControlFlowGraphView < Java::javafx.scene.control.ListView
-  def initialize
-    # TODO iterate cfg sorted_basic_blocks
-    # then create for each basic_block a BasicBlockView as Cell within this view
-    # use the factory thing for cell creation
+  def initialize(cfg)
+    @cfg = cfg
+    @cfg.sorted_basic_blocks.each do |bb|
+      # TODO create observable_list of basicblockView
+    end
+    # TODO inject observable_list to list_view constructor
+    #super(bb_cells)
   end
+  
 end
