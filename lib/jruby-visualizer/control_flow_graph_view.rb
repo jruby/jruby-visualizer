696a707dc3994b076b473ae6af3a3d71ca60f8cf
Maximilian Konzack <m.konzack@gmx.de>
2013-08-27 11:26:54 +0200

    Draft for simple textarea as list cell

--- lib/jruby-visualizer/control_flow_graph_view.rb
+++ lib/jruby-visualizer/control_flow_graph_view.rb
@@ -1,13 +1,30 @@
 require 'jrubyfx'
 
 class BasicBlockListCell < Java::javafx.scence.control.ListCell
-  def initialize
+  include JRubyFX
+  
+  def initialize(basic_block, cfg)
     # TODO compose with a textarea and buttons for successors of this basic block
+    @basic_block = basic_block
+    @instrs_box = TextArea.new(@basic_block.to_string_instrs)
+    # TODO group instrs_box and successors as VBox
+    @successors = Array.new(cfg.outgoing_destinations(@basic_block))
   end
   
-  def updateItem
+  def updateItem(item, empty)
+    super(item, empty)
+    
     # TODO update layout with a new layout manager
     # |bb_instrs|links to successors|
+    if empty
+      set_text(nil)
+      set_graphic(nil)
+    else
+      set_text(@basic_block.to_s)
+      # TODO use custom layout on instrs and successors
+      set_graphic(@instrs_box)
+    end
+    
   end
   
 end
