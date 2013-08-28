66548dfffb502b9e334f0cfe2a54a6671fff7dfa
Maximilian Konzack <m.konzack@gmx.de>
2013-08-28 16:07:40 +0200

    Changed CFGView to a composition of BorderPane and inside of it a list view; Clicking on buttons works

--- lib/jruby-visualizer/control_flow_graph_view.rb
+++ lib/jruby-visualizer/control_flow_graph_view.rb
@@ -5,13 +5,9 @@ class BasicBlockListCell < Java::javafx.scene.control.ListCell
   
   def initialize(basic_block, cfg)
     super()
-    # TODO compose with a textarea and buttons for successors of this basic block
-    @basic_block = basic_block
-    p(@basic_block.to_string_instrs)
-    p(@basic_block.to_s)
-    @instrs_box = TextArea.new(@basic_block.to_string_instrs)
-    # TODO group instrs_box and successors as VBox
-    @successors = cfg.get_outgoing_destinations(@basic_block)
+    
+    @bb_box = BasicBlockBox.new(basic_block, cfg, list_view)
+    set_item(@bb_box)
   end
   
   def updateItem(item, empty)
@@ -20,12 +16,12 @@ class BasicBlockListCell < Java::javafx.scene.control.ListCell
     # TODO update layout with a new layout manager
     # |bb_instrs|links to successors|
     if empty
-      set_text(nil)
-      set_graphic(nil)
+      #set_text(nil)
+      #set_graphic(nil)
     else
-      set_text(@basic_block.to_string_instrs)
+      #set_text(@bb_box.basic_block.to_s)
       # TODO use custom layout on instrs and successors
-      set_graphic(@instrs_box)
+      #set_graphic(@bb_box)
     end
   end
   
@@ -36,13 +32,20 @@ class BasicBlockBox < Java::javafx.scene.layout.VBox
   
   attr_reader :basic_block, :instrs_box, :successors, :successor_buttons
   
-  def initialize(basic_block, cfg)
+  def initialize(basic_block, cfg, cfg_list_view)
     super(5)
     @basic_block = basic_block
     @instrs_box = TextArea.new(@basic_block.to_string_instrs)
     @successors = cfg.get_outgoing_destinations(@basic_block).to_a
     unless @successors.empty?
-      @successor_buttons = @successors.map { |bb| Button.new(bb.to_s) }
+      @successor_buttons = @successors.map do |bb|
+        button = Button.new(bb.to_s)
+        button.set_on_action do
+          p(cfg_list_view)
+          p(cfg_list_view.class)
+        end
+        button
+      end
     
       successors_layout = HBox.new(5)
       successors_layout.get_children << Label.new("Successors: ")
@@ -56,18 +59,22 @@ class BasicBlockBox < Java::javafx.scene.layout.VBox
   
 end
 
-class ControlFlowGraphView < Java::javafx.scene.control.ListView
+class ControlFlowGraphView < Java::javafx.scene.layout.BorderPane
   include JRubyFX
   
   def initialize(cfg)
+    super()
+    
     @cfg = cfg
+    @cfg_list_view = ListView.new()
     @bb_cells = FXCollections.observable_array_list([])
     @cfg.sorted_basic_blocks.each do |bb|
       #bb_cell = BasicBlockListCell.new(bb, cfg)
-      bb_cell = BasicBlockBox.new(bb, cfg)
+      bb_cell = BasicBlockBox.new(bb, cfg, @cfg_list_view)
       @bb_cells << bb_cell
     end
-    super(@bb_cells)
+    @cfg_list_view.set_items(@bb_cells)
+    set_center(@cfg_list_view)
   end
   
 end
