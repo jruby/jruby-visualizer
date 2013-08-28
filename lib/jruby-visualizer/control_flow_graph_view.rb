0e71b567daa17ae49474b2939c75cb287c43394a
Maximilian Konzack <m.konzack@gmx.de>
2013-08-28 14:27:37 +0200

    Created custom intermediate layouter for basic blocks; successors of basic blocks are displayed as a big Label

--- lib/jruby-visualizer/control_flow_graph_view.rb
+++ lib/jruby-visualizer/control_flow_graph_view.rb
@@ -8,6 +8,7 @@ class BasicBlockListCell < Java::javafx.scene.control.ListCell
     # TODO compose with a textarea and buttons for successors of this basic block
     @basic_block = basic_block
     p(@basic_block.to_string_instrs)
+    p(@basic_block.to_s)
     @instrs_box = TextArea.new(@basic_block.to_string_instrs)
     # TODO group instrs_box and successors as VBox
     @successors = cfg.get_outgoing_destinations(@basic_block)
@@ -22,11 +23,29 @@ class BasicBlockListCell < Java::javafx.scene.control.ListCell
       set_text(nil)
       set_graphic(nil)
     else
-      set_text(@basic_block.to_s)
+      set_text(@basic_block.to_string_instrs)
       # TODO use custom layout on instrs and successors
       set_graphic(@instrs_box)
     end
-    
+  end
+  
+end
+
+class BasicBlockView < Java::javafx.scene.layout.VBox
+  include JRubyFX
+  
+  def initialize(basic_block, cfg)
+    super(5)
+    @basic_block = basic_block
+    p(@basic_block.to_string_instrs)
+    p(@basic_block.to_s)
+    @instrs_box = TextArea.new(@basic_block.to_string_instrs)
+    # TODO group instrs_box and successors as VBox
+    @successors = cfg.get_outgoing_destinations(@basic_block)
+    successors_names = @successors.inject("successors: ") do |res,bb|  
+      res + ' ' + bb.to_s
+    end
+    get_children.add_all(@instrs_box, Label.new(successors_names))
   end
   
 end
@@ -38,7 +57,9 @@ class ControlFlowGraphView < Java::javafx.scene.control.ListView
     @cfg = cfg
     @bb_cells = FXCollections.observable_array_list([])
     @cfg.sorted_basic_blocks.each do |bb|
-      bb_cell = BasicBlockListCell.new(bb, cfg)
+      #bb_cell = BasicBlockListCell.new(bb, cfg)
+      bb_cell = BasicBlockView.new(bb, cfg)
+      #bb_cell = TextArea.new(bb.to_string_instrs)
       @bb_cells << bb_cell
     end
     super(@bb_cells)
