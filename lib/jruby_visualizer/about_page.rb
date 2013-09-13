5dabcdc4ab69530168bf46366df33c7069438813
Maximilian Konzack <m.konzack@gmx.de>
2013-09-13 17:33:26 +0200

    Website loading functionality

--- lib/jruby_visualizer/about_page.rb
+++ lib/jruby_visualizer/about_page.rb
@@ -4,12 +4,27 @@ resource_root :images, File.join(File.dirname(__FILE__), "ui", "img"), "ui/img"
 fxml_root File.join(File.dirname(__FILE__), "ui")
 
 class AboutPage < JRubyFX::Application
+  
   def start(stage)
     with(stage, title: "About the JRuby Visualizer") do
       fxml(AboutPageController)
       icons.add(Image.new(resource_url(:images, "jruby-icon-32.png").to_s))
-      show
     end
+    
+    stage['#jruby_logo'].set_on_mouse_clicked do |e|
+        open_browser_with("http://jruby.org")
+      end
+    stage['#jruby_hyperlink'].set_on_action do |e|
+        open_browser_with("http://jruby.org")
+      end
+    stage['#visualizer_hyperlink'].set_on_action do |e|
+      open_browser_with("https://github.com/jruby/jruby-visualizer")
+    end
+    stage.show
+  end
+  
+  def open_browser_with(url)
+    get_host_services.show_document(url)
   end
   
 end
